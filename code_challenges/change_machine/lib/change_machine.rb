c04125f5dea56957ab2b5489c086ffe34dacfb1e
brittlewis12 <brittlewis12@gmail.com>
2014-01-28 12:19:20 -0500

    wrapped change machine api into ChangeMachine class; refactored count_change; removed currency_set from make_change to be available instance-wide

--- code_challenges/change_machine/lib/change_machine.rb
+++ code_challenges/change_machine/lib/change_machine.rb
@@ -1,18 +1,23 @@
-def make_change(cents)
-  return "Please enter a number of cents below 100." if cents >= 100
-  currency_set = [25, 10, 5, 1]
+class ChangeMachine
+  attr_reader :currency_set
 
-  currency_set.map do |coin|
-      coins = cents / coin
-      cents %= coin
-      coins
+  def initialize(args)
+    @currency_set = args[:currency_set]
   end
-end
 
-def count_change(args)
-  currency_set = [25, 10, 5, 1]
-  change = args[:quarters] * 25
-  change += args[:dimes] * 10
-  change += args[:nickels] * 5
-  change += args[:pennies]
+  def make_change(cents)
+    return "Please enter a number of cents below 100." if cents >= 100
+
+    currency_set.map do |coin|
+        coins = cents / coin
+        cents %= coin
+        coins
+    end
+  end
+
+  def count_change(args)
+    change_given = [args.fetch(:quarters, 0), args.fetch(:dimes, 0), args.fetch(:nickels, 0), args.fetch(:pennies, 0)]
+
+    change_given.zip(currency_set).reduce(0){|init, (num_coins, coin)| num_coins * coin + init}
+  end
 end
--- code_challenges/change_machine/spec/change_machine_spec.rb
+++ code_challenges/change_machine/spec/change_machine_spec.rb
@@ -1,38 +1,51 @@
 require "spec_helper"
 require_relative "../lib/change_machine"
 
-describe "#make_change" do
-  it "takes a number of cents as a argument" do
-    expect{make_change}.to raise_error ArgumentError
+describe ChangeMachine do
+  let!(:cm){ChangeMachine.new({currency_set: [25, 10, 5, 1]})}
+
+  describe "#make_change" do
+    it "takes a number of cents as a argument" do
+      expect{cm.make_change}.to raise_error ArgumentError
+    end
+
+    it "returns a currency set" do
+      expect(cm.make_change(10)).to be_an_instance_of Array
+    end
+
+    it "makes change correctly with least number of coins" do
+      expect(cm.make_change(1)).to eq [0, 0, 0, 1]
+      expect(cm.make_change(5)).to eq [0, 0, 1, 0]
+      expect(cm.make_change(10)).to eq [0, 1, 0, 0]
+      expect(cm.make_change(25)).to eq [1, 0, 0, 0]
+      expect(cm.make_change(31)).to eq [1, 0, 1, 1]
+    end
+
+    it "makes change up to 100 cents" do
+      expect(cm.make_change(101)).to eq("Please enter a number of cents below 100.")
+    end
   end
 
-  it "returns a currency set" do
-    expect(make_change(10)).to be_an_instance_of Array
-  end
+  describe "#count_change" do
+    it "takes a currency set" do
+      expect{cm.count_change}.to raise_error ArgumentError
+    end
 
-  it "makes change correctly with least number of coins" do
-    expect(make_change(1)).to eq [0, 0, 0, 1]
-    expect(make_change(5)).to eq [0, 0, 1, 0]
-    expect(make_change(10)).to eq [0, 1, 0, 0]
-    expect(make_change(25)).to eq [1, 0, 0, 0]
-    expect(make_change(31)).to eq [1, 0, 1, 1]
-  end
+    it "returns total number of cents" do
+      expect(cm.count_change({quarters: 1, dimes: 3, nickels: 3, pennies: 6})).to be_an Integer
+    end
 
-  it "makes change up to 100 cents" do
-    expect(make_change(101)).to eq("Please enter a number of cents below 100.")
-  end
-end
+    it "takes does not depend on argument order" do
+      expect(cm.count_change({pennies: 6, dimes: 3, quarters: 1, nickels: 3})).to eq 76
+    end
 
-describe "#count_change" do
-  it "takes a currency set" do
-    expect{count_change}.to raise_error ArgumentError
-  end
+    it "counts change correctly" do
+      expect(cm.count_change({pennies: 6, dimes: 3, quarters: 1, nickels: 3})).to eq 76
+      expect(cm.count_change({quarters: 2, nickels: 0, pennies: 3, dimes: 1})).to eq 63
+    end
 
-  it "returns a number of cents" do
-    expect(count_change({pennies: 6, dimes: 3, quarters: 1, nickels: 3})).to be_an Integer
-  end
-
-  it "counts change correctly" do
-    expect(count_change({pennies: 6, dimes: 3, quarters: 1, nickels: 3})).to eq 76
+    it "handles when a particular coin is not specified, defaulting to 0" do
+      expect(cm.count_change({quarters: 3, pennies: 4})).to eq 79
+    end
   end
 end
